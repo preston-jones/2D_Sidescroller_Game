2a8d36b7fb33f3048c3f068399e0b96b48b21616
preston-jones <preston.jones@gmx.de>
2024-05-16 14:30:13 +0200

    editing css mobile controller and header

--- js/models/Level.class.js
+++ js/models/Level.class.js
@@ -29,17 +29,17 @@ class Level {
 
     /**
      * The constructor of the Level class.
-     * @param {Array of objects[]} enemies - Array of enemy objects in the level.
-     * @param {Array of objects[]} animatedObjectFront - Array of animated objects in the front of the level.
-     * @param {Array of objects[]} backgroundImageStatic - Array of objects containing static background images of the level.
-     * @param {Array of objects[]} animatedBackgroundBack - Array of objects containing animated background images in the back of the level.
-     * @param {Array of objects[]} animatedBackgroundFront - Array of objects containing animated background object in the front of the level.
-     * @param {Array of objects[]} playground - Array of objects containing the ground platform object of the level.
-     * @param {Array of objects[]} collectibles_energy - Array of objects containing all collectible energy objects in the level.
-     * @param {Array of objects[]} collectibles_health - Array of objects containing all collectible health objects in the level.
-     * @param {Array of objects[]} gameOver - Array of objects containing the game over object of the level.
-     * @param {Array of objects[]} victory - Array of objects containing the victory object of the level.
-     * @param {Array of objects[]} fireworks - Array of objects containing the fireworks object of the level.
+     * @param {object[]} enemies - Array of enemy objects in the level.
+     * @param {object[]} animatedObjectFront - Array of animated objects in the front of the level.
+     * @param {object[]} backgroundImageStatic - Array of objects containing static background images of the level.
+     * @param {object[]} animatedBackgroundBack - Array of objects containing animated background images in the back of the level.
+     * @param {object[]} animatedBackgroundFront - Array of objects containing animated background object in the front of the level.
+     * @param {object[]} playground - Array of objects containing the ground platform object of the level.
+     * @param {object[]} collectibles_energy - Array of objects containing all collectible energy objects in the level.
+     * @param {object[]} collectibles_health - Array of objects containing all collectible health objects in the level.
+     * @param {object[]} gameOver - Array of objects containing the game over object of the level.
+     * @param {object[]} victory - Array of objects containing the victory object of the level.
+     * @param {object[]} fireworks - Array of objects containing the fireworks object of the level.
      */
     constructor(enemies, animatedObjectFront, backgroundImageStatic, animatedBackgroundBack, animatedBackgroundFront, playground, collectibles_energy, collectibles_health, gameOver, victory, fireworks) {
 
